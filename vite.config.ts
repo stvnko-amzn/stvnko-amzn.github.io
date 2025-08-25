18e7df31d85147829f81326b4475d633a1e848bb
SK <stvnko@amazon.com>
2025-08-25 14:13:06 -0700

    Configure GitHub Pages deployment with Vite and GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/stvnko-amzn.github.io/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
